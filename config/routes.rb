58b28c3f51272b5d747795d8e8792083c5d77a1d
satukun <y.freestyle@gmail.com>
2021-10-23 10:03:19 +0900

    first

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get '/' => 'home#top'
-  get 'about' => 'home#about'
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/create" => "posts#create"
+  
+  get "/" => "home#top"
+  get "about" => "home#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
